9948aba196583e83070cdd0e7514240e4c97eb61
IvanSaRo <ivansancrodr@gmail.com>
2021-11-19 10:36:57 +0100

    feat: style & detail added

--- src/components/ListView.js
+++ src/components/ListView.js
@@ -6,22 +6,28 @@ import { Search } from './Search'
 export const ListView = () => {
   const [data, setData] = useState(null)
 
+  const handleInputChange = ({ target }) => {
+    if (target.value.length <= 1) { return null }
+    const dataFiltered = data.filter(dat => { return dat.brand.toLowerCase().includes(target.value) || dat.model.toLowerCase().includes(target.value) })
+    setData(dataFiltered)
+  }
+
   useEffect(() => {
     getData().then((response) => {
-      // console.log(response)
       setData(response.data)
     })
   }, [])
 
   return (
-    <div>
-      <Search />
-      <h1>ListView</h1>
-      {data?.map((item) => {
-        return (
-          <Item key={item.id} {...item} />
-        )
-      })}
+    <div className='listView'>
+      <div className='listView__search'><Search handleInputChange={handleInputChange} /></div>
+      <div className='listView__items'>
+        {data?.map((item) => {
+          return (
+            <Item key={item.id} {...item} />
+          )
+        })}
+      </div>
     </div>
   )
 }
--- src/pages/ProductDetailsPage.js
+++ src/pages/ProductDetailsPage.js
@@ -4,46 +4,102 @@ import { getItemById } from '../services/service'
 
 export const ProductDetailsPage = () => {
   const [data, setData] = useState(null)
+  const [colorData, setColorData] = useState(null)
+  const [storageData, setStorageData] = useState(null)
+  const [product, setProduct] = useState({ id: null, colorCode: null, storageCode: null })
+  console.log(product)
+
   const { id } = useParams()
+
+  const handleChangeSelect = (e) => {
+    console.log(e.target.id)
+  }
+  const handleChangeStorage = (e) => {
+    console.log(e.target.id)
+  }
+
+  const handleClick = (e) => {
+    setProduct({
+      id: id,
+      colorCode: colorData,
+      storageCode: storageData
+    })
+  }
 
   useEffect(() => {
     getItemById(id).then((response) => {
       setData(response.data)
+      const colorSelect = document.getElementById('color-select')
+      const storageSelect = document.getElementById('color-storage')
+      setColorData(colorSelect.value)
+      setStorageData(storageSelect.value)
     })
   }, [])
   return (
-    <div>
-      <h1>ProductDetailsPage</h1>
-      <img src={data?.imgUrl} alt={data?.model} />
-      <ul>
-        <li><strong>Marca</strong>: {data?.brand}</li>
-        <li><strong>Modelo</strong>: {data?.model}</li>
-        <li><strong>Precio</strong>: {data?.price}€</li>
-        <li><strong>CPU</strong>: {data?.cpu}</li>
-        <li><strong>RAM</strong>: {data?.ram}</li>
-        <li><strong>Sistema Operativo</strong>: {data?.os}</li>
-        <li><strong>Resolucion de pantalla</strong>: {data?.displayResolution}</li>
-        <li><strong>Bateria</strong>: {data?.battery}</li>
-        <li><strong>Cámaras</strong>: {data?.primaryCamera}/{data?.secondasryCamera}</li>
-        <li><strong>Dimensiones</strong>: {data?.dimentions}</li>
-        <li><strong>Peso</strong>:  {data?.weight} </li>
-      </ul>
-      <div>
-        {
-          data?.options.colors.map(color => {
-            return <div key={color.code}>{color.name}</div>
-          })
-        }
+    <div className='productDetailsPage'>
+      <div className='productDetailsPage__imglink'>
+        <img src={data?.imgUrl} alt={data?.model} />
+        <Link to='/'>Volver</Link>
       </div>
-      <div>
-        {
-          data?.options.storages.map(storage => {
-            return <div key={storage.code}>{storage.name}</div>
-          })
-        }
+      <div className='productDetailsPage__data'>
+        <ul className='productDetailsPage__data-char'>
+          <li>
+            <strong>Marca</strong>: {data?.brand}
+          </li>
+          <li>
+            <strong>Modelo</strong>: {data?.model}
+          </li>
+          <li>
+            <strong>Precio</strong>: {data?.price}€
+          </li>
+          <li>
+            <strong>CPU</strong>: {data?.cpu}
+          </li>
+          <li>
+            <strong>RAM</strong>: {data?.ram}
+          </li>
+          <li>
+            <strong>Sistema Operativo</strong>: {data?.os}
+          </li>
+          <li>
+            <strong>Resolucion de pantalla</strong>: {data?.displayResolution}
+          </li>
+          <li>
+            <strong>Bateria</strong>: {data?.battery}
+          </li>
+          <li>
+            <strong>Cámaras</strong>: {data?.primaryCamera}/
+            {data?.secondasryCamera}
+          </li>
+          <li>
+            <strong>Dimensiones</strong>: {data?.dimentions}
+          </li>
+          <li>
+            <strong>Peso</strong>: {data?.weight}{' '}
+          </li>
+        </ul>
+        <div className='productDetailsPage__data-colors-container'>
+          <div>Colores: </div>
+          <select onChange={handleChangeSelect} id='color-select'>
+            {data?.options.colors.map((color) => {
+              return (
+                <option key={color.code} value={color.code}>{color.name}</option>
+              )
+            })}
+          </select>
+        </div>
+        <div className='productDetailsPage__data-storage-container'>
+          <div>Almacenamiento: </div>
+          <select onChange={handleChangeStorage} id='color-storage'>
+            {data?.options.storages.map((storage) => {
+              return (
+                <option key={storage.code} value={storage.code}>{storage.name}</option>
+              )
+            })}
+          </select>
+        </div>
+        <button className='btn btn-success' onClick={handleClick}>Añadir a cesta</button>
       </div>
-      <button className='btn btn-success'>Añadir a cesta</button>
-      <Link to='/'>Volver</Link>
     </div>
   )
 }
